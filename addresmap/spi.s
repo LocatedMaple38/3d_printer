34087977f20ba3a660b4d4f170e21a5f2bea364d
LocatedMaple38 <144717449+LocatedMaple38@users.noreply.github.com>
2026-09-22 12:42:21 -0600

    Rename filiment_hw variables and add new ones

--- addresmap/spi.s
+++ addresmap/spi.s
@@ -1,4 +1,8 @@
-filiment_hw_0 = #$00
-filiment_hw_1 = #$01
-filiment_hw_2 = #$02
-filiment_hw_3 = #$03
+filiment_0 = #$00
+filiment_1 = #$01
+filiment_2 = #$02
+filiment_3 = #$03
+filiment_4 = #$04
+filiment_5 = #$05
+filiment_6 = #$06
+display = #$07
