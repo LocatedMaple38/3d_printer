43a40f08e262c084e2b1758a19b96b81ee80053d
LocatedMaple38 <144717449+LocatedMaple38@users.noreply.github.com>
2026-09-22 13:01:26 -0600

    Update main.s

--- ams/main.s
+++ ams/main.s
@@ -1,1 +1,22 @@
+.SETCPU 65C02
+.INCLUDE ../addressmap/*.s
+.INCLUDE irq.s
+.SEGMENT CODE
+rest:
+  sei
+  cld
+  ldx #$00
+  txs
+  lda #%11111111
+  sta DDRB0
+  lda #$03
+  sta DDRA0
+  
+  
+loop:
+  jmp loop
 
+.SEGMENT RESETVEC
+  .word nmi
+  .word reset
+  .word IRQ
