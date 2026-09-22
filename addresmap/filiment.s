a2da73720affe944ab7b387bd81afed46723a239
LocatedMaple38 <144717449+LocatedMaple38@users.noreply.github.com>
2026-09-22 12:53:49 -0600

    Refactor filament parameters in filiment.s

--- addresmap/filiment.s
+++ addresmap/filiment.s
@@ -1,17 +1,19 @@
 verion = $00
-material = $02
-material_mod = $07
+material_name = $02
 manufacturer = $0C
 color_name = $1C
 color_1 = $3C
 color_2 = $40
 color_3 = $44
 color_4 = $48
-temp_nozzle_target = $
-temp_nozzle_min = $
-temp_nozzle_max = $
-temp_bed_target = $
-temp_bed_min = $
-temp_bed_max = $
+fil_dir = $8c
+temp_nozzle_tar = $90
+temp_nozzle_min = $91
+temp_nozzle_max = $92
+temp_chambr_tar = 93
+temp_bed_tar = $94
+temp_bed_min = $95
+temp_bed_max = $96
+weight = $9e
 
-filiment_ram = $02
+filiment_ram_0 
