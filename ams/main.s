e61a58c89ae91baa22c7f136744453dd3da155b3
LocatedMaple38 <144717449+LocatedMaple38@users.noreply.github.com>
2026-09-23 10:51:23 -0600

    Refactor assembly code for clarity and comments

--- ams/main.s
+++ ams/main.s
@@ -5,20 +5,21 @@
 rest:
   sei
   cld
-  ldx #$00
-  txs
-  lda #%11111111
+  ldx #$00                  ;load x reg with 0
+  txs                       ;and set stack pointer to x
+  lda #%11111111            ;set DDRB0 to all out puts
   sta DDRB0
-  lda #$03
+  lda #$03                  ;set DDRA0 bit 0,1,2 to output
   sta DDRA0
-  lda #%00010100
+  lda #%00010100            ;set ACR0 Shift reg to shift out undr controle of T2 
   sta ACR0
-  lda #%10000111        ;int enable, sr enable, CA1,2 enable (filimnet_0, 1)
+  lda #%10000111            ;int enable, sr, CA1,2 (filimnet_0,1)
   sta IER0
-  lda #%00001100
+  lda #%00001100            ;set ACR1 Shift reg to shift in undr control of external clock
   sta ARC1
-  lda #%10000111
+  lda #%10000111            ;int enable, SR, CA1,2 (filiment_2,3)
   sta IER1
+
   
   jmp loop
   
